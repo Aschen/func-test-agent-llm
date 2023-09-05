388046ac8d061d2adc7fbb5ff17c8d871f951c1a
Aschen <amaret93@gmail.com>
2023-09-05 02:03:02 +0200

    update run.ts

--- run.ts
+++ run.ts
@@ -6,20 +6,19 @@ import { DocumentationAgent} from "./DocumentationAgent";
 import { WriteTestsAgent } from "./WriteTestsAgent";
 
 async function run() {
-  // const extractEntrypoint = new ExtractEntrypointsAgent('./examples/fastify/app.js');
-  // await extractEntrypoint.run();
+  const extractEntrypoint = new ExtractEntrypointsAgent('./examples/fastify/app.js');
+  await extractEntrypoint.run();
 
-  // for (const entrypoint of extractEntrypoint.output) {
-    // const entrypoint = extractEntrypoint.output[3]; 
-    // const walkCallStack = new WalkCallStackAgent(entrypoint);
-    // await walkCallStack.run();
+  for (const entrypoint of extractEntrypoint.output) {
+    const walkCallStack = new WalkCallStackAgent(entrypoint);
+    await walkCallStack.run();
   
-    // const documentation = new DocumentationAgent(entrypoint, walkCallStack.output.slice(1));
-    // await documentation.run();
+    const documentation = new DocumentationAgent(entrypoint, walkCallStack.output.slice(1));
+    await documentation.run();
   
-    const writeTests = new WriteTestsAgent(readFileSync('./examples/fastify/doc/update-todos.md', 'utf-8'));
+    const writeTests = new WriteTestsAgent(documentation.output[1]);
     await writeTests.run();
-  // }
+  }
 }
 
 run()
